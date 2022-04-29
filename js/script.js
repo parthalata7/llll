aaace188e5b229f31b518cdb651fd230ffa52296
parthalata7 <100035000+parthalata7@users.noreply.github.com>
2022-04-29 15:39:17 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const newLocal = $(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
